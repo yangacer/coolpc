d86fe8c877ef12b816164596199a5ad9066a5370
yangacer <yangacer@gmail.com>
2018-11-18 21:32:03 +0800

    Support send mail.

--- coolpc.py
+++ coolpc.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 import argparse
+import base64
+import cStringIO
 import datetime as dt
+import httplib2
+import mimetypes
+import oauth2client as oauth
+import oauth2client.file
+import oauth2client.tools
+import oauth2client.client
 import os
 import shutil
 import sqlite3
 import subprocess as sp
 import sys
+
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
 
 def matchTag(s, tag):
   '''
@@ -70,8 +85,8 @@ def getToks(s):
 
 def userDataDir():
   map = {
-    'nt': os.environ['LOCALAPPDATA'],
-    'posix': os.environ['HOME']
+    'nt': os.environ.get('LOCALAPPDATA', None),
+    'posix': os.environ.get('HOME', None)
   }
   return os.path.join(map[os.name], '.coolpc')
 
@@ -100,7 +115,7 @@ class DB(object):
     db_csr.execute('select L.*, (((L.price - P.price) * 100.0)/ P.price) as Diff'
                    ' from LatestPrice as L join PreviousPrice as P on'
                    ' (L.product == P.product and L.price != P.price)')
-    print db_csr.fetchall()
+    return db_csr.fetchall()
 
 def install(reset=False):
   user_dir = userDataDir()
@@ -123,6 +138,50 @@ def fetchPage():
                           '--enable-logging',
                           'https://www.coolpc.com.tw/evaluate.php'])
 
+class MailAgent(object):
+  api_scope = 'https://www.googleapis.com/auth/gmail.send'
+  def __init__(self):
+    sys.argv = []
+    store = oauth.file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = oauth.client.flow_from_clientsecrets('credentials.json',
+                                                    MailAgent.api_scope)
+        creds = oauth.tools.run_flow(flow, store)
+    self.service = build('gmail', 'v1', http=creds.authorize(httplib2.Http()))
+
+  def createMessage(self, sender, to, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+  def sendTest(self):
+    msg = self.createMessage(
+      'yangacer@gmail.com',
+      'yangacer@gmail.com',
+      'Coolpc test mail',
+      'This is a test mail from coolpc crawler.')
+    self.send(msg)
+
+  def sendDiff(self, diff):
+    msg = self.createMessage(
+      'yangacer@gmail.com',
+      'yangacer@gmail.com',
+      '[Coolpc] Price Watchlist',
+      diff)
+    self.send(msg)
+
+
+  def send(self, message):
+    try:
+      resp = (self.service.users().messages().send(userId='me', body=message)
+                 .execute())
+      print 'Message Id: %s' % resp['id']
+    except errors.HttpError, error:
+      print 'An error occurred: %s' % error
+
 def parseArgs():
   parser = argparse.ArgumentParser()
   parser.add_argument('file', nargs='?', help='Read web page from file',
@@ -132,18 +191,27 @@ def parseArgs():
   parser.add_argument('--dryrun', '-d', action='store_true')
   parser.add_argument('--verbose', '-v', action='store_true')
   parser.add_argument('--diff', action='store_true')
+  parser.add_argument('--testmail', action='store_true')
   return parser.parse_args()
 
 def main():
   args = parseArgs()
-  db = DB()
 
   if args.install is True:
     install()
     return
 
+  db = DB()
+  ma = MailAgent()
+
   if args.diff is True:
-    db.computeDiff()
+    diff = db.computeDiff()
+    for cate, vendor, prod, note, price, date, diff in diff:
+      print prod, price, diff
+    return
+
+  if args.testmail is True:
+    ma.sendTest()
     return
 
   now = dt.datetime.now()
@@ -170,6 +238,16 @@ def main():
 
   if args.dryrun is False:
     db.update(data)
+    diff = db.computeDiff()
+    if len(diff) > 0:
+      output = cStringIO.StringIO()
+      for cate, vendor, prod, note, price, date, diff in diff:
+        print >>output, vendor, prod, price, diff
+      ma.sendDiff(output.getvalue())
+      output.close()
+    else:
+      print 'No price diff found'
+
 
 if __name__ == '__main__':
   script_dir = os.path.dirname(os.path.abspath(__file__))
